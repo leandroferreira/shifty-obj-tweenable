f101d02e2e83bc30cee6fa56f40bc3eadb20b1bd
Leandro Ferreira <durang@gmail.com>
2014-11-21 15:38:32 -0200

    added support for AMD

--- obj-tweenable.js
+++ obj-tweenable.js
@@ -1,76 +1,50 @@
-(function (exports) {
+(function (global) {
   'use strict';
 
-  /**
-   * ObjTweenable is Tweenable applying to any object variables.
-   * Based heavily on Zain Manji's ShiftyTimelineAnimation
-   * @param {Element} element Any object
-   * @param {Object} tweenConfig Tween configuration as per Shifty
-   */
-  var ObjTweenable = exports.ObjTweenable = function(element, tweenConfig) {
-    var step = tweenConfig.step || function() {};
-    tweenConfig.step = function(state) {
-      Object.keys(state).forEach(function (key) {
-        element[key] = state[key];
-      });
-      step.apply(this, arguments);
+  var objTweenable = function (root, Tweenable) {
+    /**
+     * ObjTweenable is Tweenable applying to any object variables.
+     * Based heavily on Zain Manji's ShiftyTimelineAnimation
+     * @param {Object} element Any object
+     * @param {Object} tweenConfig Tween configuration as per Shifty
+     */
+    var ObjTweenable = function(element, tweenConfig) {
+      var step = tweenConfig.step || function() {};
+      tweenConfig.step = function(state) {
+        Object.keys(state).forEach(function (key) {
+          element[key] = state[key];
+        });
+        step.apply(this, arguments);
+      };
+      this.tweenConfig = tweenConfig;
+      Tweenable.call(this, {}, tweenConfig);
     };
-    this.tweenConfig = tweenConfig;
-    Tweenable.call(this, {}, tweenConfig);
-  };
 
-  ObjTweenable.prototype = Object.create(Tweenable.prototype, {
-    constructor: {
-      value: ObjTweenable,
-      enumerable: false,
-      writable: true,
-      configurable: true
-    }
-  });
-
-  /**
-   * Timeline is Tweenable instance able to contains sub tweens who'll
-   * start at a specified delay.
-   * @param {Object} tweenConfig Tween configuration as per Shifty
-   */
-  var Timeline = exports.Timeline = function(tweenConfig) {
-    this.tweenableList = [];
-    this.tweenConfig = tweenConfig || {};
-    tweenConfig.step = function(state, obj, frame) {
-      this._updateSubtweenables(frame);
-    }.bind(this);
-    Tweenable.call(this, {}, tweenConfig);
-  };
-
-  Timeline.prototype = Object.create(Tweenable.prototype, {
-    constructor: {
-      value: Timeline,
-      enumerable: false,
-      writable: true,
-      configurable: true
-    }
-  });
-
-  /**
-   * Register a sub tween on the timeline
-   * @param {Tweenable} tweenable A Tweenable instance
-   * @param {Number} delay        Delay after which the tween should start
-   */
-  Timeline.prototype.add = function(tweenable, delay) {
-    delay = delay || 0;
-
-    this.tweenableList.push({ tweenable: tweenable, delay: delay });
-
-    var tweenDuration = tweenable.tweenConfig.duration + delay;
-    if (tweenDuration > this.tweenConfig.duration) {
-      this.tweenConfig.duration = tweenDuration;
-      this.setConfig(this.tweenConfig);
-    }
-  };
-
-  Timeline.prototype._updateSubtweenables = function(frame) {
-    this.tweenableList.forEach(function(timelineObj) {
-      timelineObj.tweenable.seek(frame - timelineObj.delay);
+    ObjTweenable.prototype = Object.create(Tweenable.prototype, {
+      constructor: {
+        value: ObjTweenable,
+        enumerable: false,
+        writable: true,
+        configurable: true
+      }
     });
+
+    return ObjTweenable;
   };
+
+  /**
+   * Exports to multiple envs
+   */
+
+  if(typeof define === 'function' && define.amd) { //AMD
+    define('obj-tweenable', ['shifty'], function (Tweenable) {
+      return objTweenable({}, Tweenable);
+    });
+  } else if (typeof module !== 'undefined' && module.exports) { //node
+    module.exports = {ObjTweenable: objTweenable(global, global.Tweenable)};
+  } else { //browser
+    //use string because of Google closure compiler ADVANCED_MODE
+    /*jslint sub:true */
+    global['ObjTweenable'] = objTweenable(global, global.Tweenable);
+  }
 })(this);
